fdcc52030fcd761058eb48f56f99e19e27b02576
= <=>
2025-06-30 21:30:18 +0900

    feat: Create pizza point api for frontend
    - frontend 시각화를 위한 자료 제공 api 구축

--- prisma/migrations/20250630115553_add_point_metric_table/migration.sql
+++ prisma/migrations/20250630115553_add_point_metric_table/migration.sql
@@ -25,5 +25,16 @@ CREATE TABLE `ShopMetric` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `PointMetric` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` DATETIME(3) NOT NULL,
+    `timeSlot` VARCHAR(191) NOT NULL,
+    `point` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `ShopMetric` ADD CONSTRAINT `ShopMetric_shopId_fkey` FOREIGN KEY (`shopId`) REFERENCES `PizzaShop`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
